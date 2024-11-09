b51969bcd048f6406cd4c9ac09c5c2435fe402d1
Inshaal <139726478+inshaalc@users.noreply.github.com>
2024-11-09 01:16:39 -0500

    Update CourseDBStructure.java

--- Assignment4/CourseDBStructure.java
+++ Assignment4/CourseDBStructure.java
@@ -6,153 +6,195 @@
  * Due: 10/27/2024
  * Platform/compiler: Eclipse
  * I pledge that I have completed the programming  assignment independently. 
-*  I have not copied the code from a student or any source. 
-*  I have not given my code to any student.
-*  Print your Name here: Inshaal Chaudhury
-*/
+ * I have not copied the code from a student or any source. 
+ * I have not given my code to any student.
+ * Print your Name here: Inshaal Chaudhury
+ */
 /*
  * @author Inshaal Chaudhury
  */
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.LinkedList;
 
-public class CourseDBStructure implements CourseDBStructureInterface 
+public class CourseDBStructure implements CourseDBStructureInterface
 {
     // Attributes
-    public LinkedList<CourseDBElement>[] table;
-
-    // Constructor One
+    public int numberCourses;
+    private final double MAX_LOADING_FACTOR = 1.5;
+    public LinkedList<CourseDBElement>[] hashTable;
+    private int tableSize;
+    private String testing;
+    
+    /**
+     * A constructor that takes in an integer n which represents the estimated number of 
+     * courses and determines the size of the hash table by finding a 4K+3 prime just greater than n /loading factor.
+     * Example:  if n is 500 courses, then 500/1.5 = 333, The next 4K+3 prime over 333 is 347.  So, you would set the table a length to 347.
+     * @param n Number of courses
+     */
     @SuppressWarnings("unchecked")
-    public CourseDBStructure(int n) 
+    public CourseDBStructure(int n)
     {
-        table = new LinkedList[n];
-        for (int i = 0; i < n; i++) 
+        // Divides number of courses by 1.5 and rounds it up
+        int targetSize = (int) Math.ceil((n) / MAX_LOADING_FACTOR);
+        // Finds next 4k+3 prime over target size
+        targetSize += 3 - (targetSize % 4);
+        
+        // If it is not prime, keeps adding 4 until next 4k+3 is found
+        while (!isPrime(targetSize))
         {
-            table[i] = new LinkedList<CourseDBElement>();
+            targetSize += 4;
+        }
+        
+        this.tableSize = targetSize;
+        
+        // Creates hashtable using a linkedList the size of the target size
+        // For every element in hashtable, courseDBElement is assigned
+        hashTable = new LinkedList[tableSize];
+        for (int i = 0; i < tableSize; i++)
+        {
+            hashTable[i] = new LinkedList<CourseDBElement>();
         }
     }
-
-    // Constructor Two
-    @SuppressWarnings("unchecked")
-    public CourseDBStructure(String testing, int n) 
+    
+    /**
+     * Helper method calculates whether targetSize is a prime number
+     * @param targetSize
+     * @return true if prime, false if not prime
+     */
+    public boolean isPrime(int n)
     {
-        table = new LinkedList[n];
-        for (int i = 0; i < n; i++) 
+        // If less than or equal to 1, not prime
+        if (n <= 1)
+            return false;
+        // If either 2 or 3, is prime
+        if (n <= 3)
+            return true;
+        // If either divisible by 2 or 3, not prime
+        if (n % 2 == 0 || n % 3 == 0)
+            return false;
+        
+        // For loop checks divisors from 5 to the square root of n to see if n is divisble by i. If so, not prime
+        for (int i = 5; i * i <= n; i += 2)
         {
-            table[i] = new LinkedList<CourseDBElement>();
+            if (n % i == 0)
+                return false;
+        }
+        
+        return true;
+    }
+    
+    /**
+     * A Constructor for testing purposes. 
+     * This constructor will take a string "Testing" and an int for the hashTable size. This is used only for testing.
+     * @param testing
+     * @param n HashTable size 
+     */
+    @SuppressWarnings("unchecked")
+    public CourseDBStructure(String testing, int n)
+    {
+        this.testing = testing;
+        tableSize = n;
+        
+        hashTable = new LinkedList[tableSize];
+        for (int i = 0; i < tableSize; i++)
+        {
+            hashTable[i] = new LinkedList<CourseDBElement>();
         }
     }
-
+    
     /** 
-    * Adds a CourseDBElement object to the CourseDBStructure using the hash code of the CRN value.
-    * If the CourseDBElement already exists, exit quietly.
-    * @param element the CourseDBElement to be added to CourseDBStructure.
-    */
+     * Adds a CourseDBElement object to the CourseDBStructure using the 
+     * hashcode of the CourseDatabaseElemen object's crn value.
+     * If the CourseDatabaseElement already exists, exit quietly
+     * @param element the CourseDBElement to be added to CourseDBStructure
+     */
     @Override
     public void add(CourseDBElement element) 
     {
-    	Integer crnAsInteger = Integer.valueOf(element.getCRN());
-    	int index = Math.abs(crnAsInteger.hashCode()) % table.length;
-    	
-    	
-    	
-        LinkedList<CourseDBElement> bucket = table[index];
-
-        if (bucket == null) 
+        int key = element.getCRN();
+        int index = Math.abs(key) % hashTable.length;
+        
+        if (hashTable[index] == null)
+            hashTable[index] = new LinkedList<>();
+        
+        for (CourseDBElement existingElement : hashTable[index])
         {
-            bucket = new LinkedList<>();
-            table[index] = bucket;
-        }
-        else
-        {
-        	for (int i = 0; i < bucket.size(); i++) 
+            if (existingElement.getCRN() == element.crn)
             {
-                if (bucket.get(i).getCRN() == element.getCRN()) 
-                {
-                    bucket.set(i, element);
-                    return;
-                }
+                hashTable[index].remove(existingElement);
+                hashTable[index].add(element);
+                return;
             }
         }
-        bucket.add(element);
-
+        
+        hashTable[index].add(element);
     }
 
     /**
-    * Find a CourseDBElement based on the key (CRN) of the CourseDBElement. 
-    * If found, return it; otherwise, throw an IOException.
-    * @param crn CRN (key) whose associated CourseDBElement is to be returned.
-    * @return a CourseDBElement whose CRN is mapped to the key.
-    * @throws IOException if the key is not found.
-    */
+     * Find a courseDatabaseElement based on the key (crn) of the
+     * courseDatabaseElement If the CourseDatabaseElement is found return it If not,
+     * throw an IOException.
+     * @param crn crn (key) whose associated courseDatabaseElement is to be returned
+     * @return a CourseDBElement whose crn is mapped to the key
+     * @throws IOException if key is not found
+     */
     @Override
     public CourseDBElement get(int crn) throws IOException 
     {
-        int hashCode = Integer.hashCode(crn);
-        int index = Math.abs(hashCode % getTableSize());
+        int index = Math.abs(crn) % tableSize;
         
-        if (index < 0 || index >= getTableSize()) 
-        {
-            throw new IOException("Invalid index calculated for CRN: " + crn);
-        }
+        LinkedList<CourseDBElement> bucket = hashTable[index];
         
-        if (table[index] == null || table[index].isEmpty()) 
-        {
-            System.out.println("No elements at index: " + index);
-        }
+        if (bucket == null)
+            throw new IOException("Course not found");
         
-
-        for (CourseDBElement element : table[index]) 
+        for (CourseDBElement element : bucket)
         {
             if (element.getCRN() == crn)
-            {
                 return element;
-            }
         }
-
-        throw new IOException("CRN not found: " + crn);
+        
+        throw new IOException("Course not found");
     }
 
     /**
-	 * @return an array list of string representation of each course in 
-	 * the data structure separated by a new line. 
-	 * Refer to the following example:
-	 * Course:CMSC500 CRN:39999 Credits:4 Instructor:Nobody InParticular Room:SC100
-	 * Course:CMSC600 CRN:4000 Credits:4 Instructor:Somebody Room:SC200
-	 */
+     * @return an array list of string representation of each course in 
+     * the data structure separated by a new line. 
+     * Refer to the following example:
+     * Course:CMSC500 CRN:39999 Credits:4 Instructor:Nobody InParticular Room:SC100
+     * Course:CMSC600 CRN:4000 Credits:4 Instructor:Somebody Room:SC200
+     */
     @Override
     public ArrayList<String> showAll() 
     {
-    	ArrayList<String> courseSchedule = new ArrayList<>();
-    	
-    	for (LinkedList<CourseDBElement> bucket : table)
-    	{
-    		if (bucket != null)
-    		{
-    			for (CourseDBElement element : bucket)
-    			{
-    				String info = "\n" + "Course:" + element.getID()
-    									+ " CRN:" + element.getCRN()
-    									+ " Credits:" + element.numCredits()
-    									+ " Instructor:" + element.getName()
-    									+ " Room:" + element.getRoomNum();
-    				courseSchedule.add(info);
-    			}
-    		}
-    	}
-    	return courseSchedule;
+        ArrayList<String> courseList = new ArrayList<>();
+        
+        for (LinkedList<CourseDBElement> bucket : hashTable)
+        {
+            if (bucket != null)
+            {
+                for (CourseDBElement element : bucket)
+                {
+                    String courseInfo = "\nCourse:" + element.getID() +
+                                        " CRN:" + element.getCRN() +
+                                        " Credits:" + element.numCredits() +
+                                        " Instructor:" + element.getName() +
+                                        " Room:" + element.getRoomNum();
+                    courseList.add(courseInfo);
+                }
+            }
+        }
+        return courseList;
     }
 
     /**
-    * Returns the size of the CourseDBStructure (number of indexes in the array).
-    */
+     * Returns the size of the ConcordanceDataStructure (number of indexes in the array)
+     */
     @Override
     public int getTableSize() 
     {
-        return table.length;
+        return tableSize;
     }
 }
